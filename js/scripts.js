2670085d4852bc14017afa0da859dd24df4b4957
David Wynn <davidwwynn@gmail.com>
2016-02-29 19:47:32 -0800

    add logic for multiple word palindromes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-var pal = function(word) {
-  console.log(word.split('').join(''));
-  console.log(word.split('').reverse().join(''));
-  if (word.split('').join('') === word.split('').reverse().join('')) {
-  console.log("it's a palindrome");
-  } else {
-  console.log("it's not a palindrome");
-  }
+// function to remove spaces
+var cleanString = function(string) {
+  return string.split(' ').join('');
 };
+
+// function to prepare original string for compare
+var prep = function(string) {
+  return cleanString(string);
+};
+
+// function to prepare flipped string
+var flip = function(string) {
+  return cleanString(string).replace(' ','').split('').reverse().join('');
+};
+
+// function isPal to evaluate word (without spaces) vs. flipped word
+var isPal = function(string) {
+  var flipString = flip(string);
+  var prepString = prep(string);
+  console.log("original string prepped = " + prepString); //original word (string)
+  console.log("flipped string = " + flipString); //flipped word as string
+  if (prepString === flipString) {
+    console.log("YES, it's a palindrome");
+    } else {
+    console.log("NO, it's not a palindrome");
+    };
+  };
